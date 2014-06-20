3344f7cd75a6e9e12d28045688d12d88de8ebea9
Dave Snider <dave.snider@gmail.com>
2014-06-20 10:12:21 -0700

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,83 @@
+'use strict';
+module.exports = function(grunt) {
+
+  // ----------------------------------------------------------
+  // WARNING, BRAVE DEVELOPER
+  // ----------------------------------------------------------
+  // Webhook allows you to use local grunt tasks and files.
+  // However, these tasks are ONLY RUN LOCALLY and not when
+  // your live site needs to be rebuilt. This means you should
+  // only use grunt for pre-processing tasks like building
+  // Sass, less or coffescript files, not for reading things
+  // from your templates and making dynamic changes during
+  // the build process. Doing so will cause your live site
+  // not to regerate.
+  //
+  // You have been warned!
+
+  grunt.initConfig({
+    // BUILD SASS TO CSS
+    sass: {
+      dev: {
+        options: {
+          // We used the expanded style because Webhook already minifies CSS when it deploys your site.
+          style: 'expanded',
+
+          // Uncomment the below line to include outside directories as well.
+          // loadPath: ['location/of/other/sass']
+        },
+        files: [{
+          // Files in the /sass/ directory will go to /static/css/ when processed.
+          expand: true,
+          cwd: 'sass',
+          src: ['*.sass'],
+          dest: 'static/css',
+          ext: '.css'
+        }]
+      }
+    },
+
+    // BUILD LESS TO CSS
+    less: {
+      dev: {
+        options: {
+          // Uncomment the below line to include outside directories as well.
+          // paths: ['location/of/other/less/']
+        },
+        files: [{
+          // Files in the /less/ directory will go to /static/css/ when processed.
+          expand: true,
+          cwd: 'less',
+          src: ['*.less'],
+          dest: 'static/css',
+          ext: '.css'
+        }]
+      }
+    },
+
+    // BUILD COFFEE TO JAVASCRIPT
+    coffee: {
+      compile: {
+        files: {
+          'static/javascript/main.js': ['coffee/*.coffee'] // compile and concat into single file
+        }
+      },
+    },
+    watch: {
+      options : {
+        files: ['sass/**/*.sass', 'less/**/*.less','bower_components/wyrm/sass/**/*.sass'],
+        tasks: ['sass', 'less', 'coffee', 'build']
+      },
+    }
+  });
+
+  // THIS LOADS THE TASKS WE NEED ABOVE IN FROM OUR NPM
+  // Note, that we need to have these installed through the package.json file as well
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+
+  // NEVER REMOVE THESE LINES, OR ELSE YOUR PROJECT MAY NOT WORK
+  require('./options/generatorOptions.js')(grunt);
+  grunt.loadTasks('tasks');
+};
---
+++ README.md
@@ -0,0 +1,8 @@
+## Webhook preprocessor theme
+
+This theme builds a grunt file and installs the needed
+Node modules to preprocess Sass, Less and Coffescript
+files.
+
+Once installed, you'll need your restart your Webhook
+runserver for the watch command to properly fire.
